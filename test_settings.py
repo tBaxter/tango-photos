4c106cc69281817810f6f308780382e4c2d9c544
TBaxter <mail.baxter@gmail.com>
2018-08-02 13:02:40 -0500

    Setup for travis

---
+++ test_settings.py
@@ -0,0 +1,33 @@
+SECRET_KEY = "lorem ipsum"
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.sites',
+    'django.contrib.sessions',
+
+    'tango_shared',
+    'easy_thumbnails',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+ROOT_URLCONF = 'photos.urls'
+
+SITE_ID = 1
+
+PAGINATE_BY = 20
+
+#stripped down middleware
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
