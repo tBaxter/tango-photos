089a1d69981f5912225b41f371a3f1775ef4cb8b
TBaxter <mail.baxter@gmail.com>
2018-08-04 16:48:24 -0500

    Url correction

--- photos/urls.py
+++ photos/urls.py
@@ -3,14 +3,14 @@ from django.views.generic import DetailView, ListView
 
 from .models import Gallery
 
+galleries = Gallery.published_objects.all()
+
 urlpatterns = [
-    path('', ListView, 
-    name="gallery_list"
-    ),
+    path('', ListView.as_view(queryset=galleries), name="gallery_list"),
     path(
         '<slug:slug>/', 
         DetailView.as_view(
-            queryset=Gallery.published_objects.all(),
+            queryset=galleries,
             template_name="galleries/gallery_detail.html"
         ),
         name="gallery_detail"
