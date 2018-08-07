6b98eb2c9f398a35d5ed7338311f03a84b895dba
TBaxter <mail.baxter@gmail.com>
2018-08-07 08:29:32 -0500

    try with context processor

--- test_settings.py
+++ test_settings.py
@@ -41,5 +41,13 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
